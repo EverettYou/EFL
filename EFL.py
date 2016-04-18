3743f3702798d93e83b6f7e422fbb3791033fb9e
EverettYou <everettyou.internet@gmail.com>
2016-04-18 12:56:52 -0700

    Repository setup

---
+++ EFL.py
@@ -0,0 +1,10 @@
+''' Entanglement Feature Learning '''
+# package required: numpy, theano
+import numpy
+import theano
+import theano.tensor as T
+import os
+''' RMB (Kernel)
+
+'''
+class RMB(object):
